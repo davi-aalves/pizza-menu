60224c29efc75005a1f1166ca18b5d244aac0d29
Davi A Alves <daviih2000@gmail.com>
2024-12-18 10:59:19 -0300

    feat: enhance Pizza component with dynamic props and additional details
    - Updated multiple <Pizza /> instances to use props for name, ingredients, photoName, and price.
    - Refactored the Pizza component to accept props and display dynamic content.
    - Added console.log for debugging props in the Pizza component.
    - Included a className for styling and added a computed price with a fixed increment.

--- src/index.js
+++ src/index.js
@@ -72,11 +72,35 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu:</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, and pepperoni"
+        photoName="pizzas/salamino.jpg"
+        price={10}
+      />
+
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
+    </div>
   );
 }
 
@@ -97,16 +121,6 @@ function Footer() {
   );
 
   // return React.createElement("footer", null, "We're currently open!");
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/salamino.jpg" alt="A salamino pizza" />
-      <h3>Pizza Salamino</h3>
-      <p>Tomato, mozarella, and pepperoni</p>
-    </div>
-  );
 }
 
 // React v18
